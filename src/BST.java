bd34c672b81ec84718a55d08139c55aac07365c4
Stephen Rowe <rowes@tcd.ie>
2020-10-09 00:44:01 +0100

    added comments for future reference

--- src/BST.java
+++ src/BST.java
@@ -10,42 +10,44 @@ public class BST {
         }
     }
 
+    // root of our tree
     Node root;
 
     public BST() {
         root = null;
     }
 
+    // call recursive function
+    public Node findLCA(int key1, int key2) { return findLCA(root, key1, key2); }
+
+    // recursive function
+    private Node findLCA(Node searchNode, int key1, int key2) {
+        if (searchNode == null) return null;        // if no root node, return null
+        if (searchNode.key == key1 || searchNode.key == key2) return searchNode;    // if key matches search node,
+                                                                                    // return search node
+        Node leftSearch = findLCA(searchNode.leftChild, key1, key2);    // call recursively on left branch
+        Node rightSearch = findLCA(searchNode.rightChild, key1, key2);  // call recursively on right branch
+
+        if (leftSearch != null && rightSearch != null) return searchNode;
+        return (leftSearch != null) ? leftSearch : rightSearch;
+    }
+
     public void addNode(int value) {
         if (root == null) root = new Node(value);
         Node searchNode = root;
         while (true) {  // if the same number is entered twice, it will not create a new node for the repetition
-            if (value == searchNode.key) break;
+            if (value == searchNode.key) break;     // break if current node has the same value
             if (value < searchNode.key) {
                 if (searchNode.leftChild == null) {
-                    searchNode.leftChild = new Node(value);
+                    searchNode.leftChild = new Node(value);  // create new left node if no left node, and breaks
                     break;
-                } else searchNode = searchNode.leftChild;
-            } else if (value > searchNode.key) {
+                } else searchNode = searchNode.leftChild;    // if left child present and not equal, search left branch
+            } else {    // if (value > searchNode.key)
                 if (searchNode.rightChild == null) {
-                    searchNode.rightChild = new Node(value);
+                    searchNode.rightChild = new Node(value); // create new right node if no left node, and breaks
                     break;
-                } else searchNode = searchNode.rightChild;
+                } else searchNode = searchNode.rightChild;   // if left child present and not equal, search left branch
             }
         }
-    }
-
-    // call recursive function
-    Node findLCA(int key1, int key2) { return findLCA(root, key1, key2); }
-
-    Node findLCA(Node searchNode, int key1, int key2) {
-        if (searchNode == null) return null;
-        if (searchNode.key == key1 || searchNode.key == key2) return searchNode;
-
-        Node leftSearch = findLCA(searchNode.leftChild, key1, key2);
-        Node rightSearch = findLCA(searchNode.rightChild, key1, key2);
-
-        if (leftSearch != null && rightSearch != null) return searchNode;
-        return (leftSearch != null) ? leftSearch : rightSearch;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 import java.util.Random;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         // BST will look like this: https://i.ibb.co/1dQdqDD/Untitled.png
         BST ourTree = new BST();
-        int [] inputValues = {15,14,28,22,9,12,30,29,4,7,13};
-        for(int i: inputValues) ourTree.addNode(i);
+        int[] inputValues = {15, 14, 28, 22, 9, 12, 30, 29, 4, 7, 13};
+        for (int i : inputValues) ourTree.addNode(i);
         BST.Node root = ourTree.root;
         System.out.println("done");
+    }
 }
